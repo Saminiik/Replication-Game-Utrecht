865dc355c217f279bdaa96f167ddf296508a0b2a
Hanne Oberman <h.i.oberman@uu.nl>
2026-06-04 14:21:40 +0200

    add R project, fix hard-coded directory

--- Code/Haryana/ecma_directory.R
+++ Code/Haryana/ecma_directory.R
@@ -1,3 +1,3 @@
 #ecmadir <- "/Users/anirudhsankar/Dropbox/Smart Pooling and Pruning/ECMAReplication/replication package/"
 
-ecmadir <-"/Users/7471882/Dropbox/PhD2/tasks/Utrecht replication game/replication package/"
+ecmadir <-"./"
